6c6b8008d3184f194a8815774d6904eee40339ef
KyDevtw <k3030789@gmail.com>
2021-06-30 00:21:18 +0800

    fuck'n SVG

--- src/pages/event/Event.js
+++ src/pages/event/Event.js
@@ -5,12 +5,22 @@ import {
   Col,
   Button,
 } from 'react-bootstrap'
-
+import './style/event.scss'
 
 function event() {
   return (
     <>
-    
+      <div className="reduce-width">
+        <div className="container-fluid">
+          <div className="row">
+            <img src="" alt="" />
+            <section className="hero">
+              <div className="spin-circle invisible"></div>
+              <div className="spin-eye invisible"></div>
+            </section>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
