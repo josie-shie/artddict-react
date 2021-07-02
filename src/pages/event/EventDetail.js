c851f1719fa6377b5b1b78b69a0e49d2c24acea8
KyDevtw <k3030789@gmail.com>
2021-07-03 00:32:54 +0800

    nav user link update

---
+++ src/pages/event/EventDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Row } from 'react-bootstrap'
+import './style/reset.css'
+import './style/fontAndBtn.scss'
+import './style/eventList.scss'
+
+function EventDetail() {
+  return <>
+    
+  </>
+}
+
+export default EventDetail
---
+++ src/pages/event/EventList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, Row } from 'react-bootstrap'
+import './style/reset.css'
+import './style/fontAndBtn.scss'
+
+function EventList() {
+  return <></>
+}
+
+export default EventList
