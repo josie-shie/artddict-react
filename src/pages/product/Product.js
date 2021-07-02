3cf59ecd7c3b715e8e4aabb2166dd5dcbadcd6ce
tascup1 <70184088+tascup1@users.noreply.github.com>
2021-07-02 16:48:43 +0800

    reactSlick and howToUse

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -3,9 +3,16 @@ import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../pics/logo.svg'
 import './style/Product.css'
 import '../../bootstrap/css/bootstrap.css'
-import { CgArrowLongRightL, CgPacman } from 'react-icons/cg'
+import { Card, Button } from 'react-bootstrap'
+import {} from 'react-icons/cg'
+import { MdKeyboardArrowRight } from 'react-icons/md'
 import { BsThreeDots } from 'react-icons/bs'
-
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+//-------test-----
+import Testcard from './compoenents/testCard'
+// -----picturre--------
 import logo from './svg/logo.svg'
 import arddictCircleG from './svg/arddictCircleG.svg'
 import arddictEyeG from './svg/arddictEyeG.svg'
@@ -15,8 +22,24 @@ import homepic2 from './img/homepic2.png'
 import homepic3 from './img/homepic3.png'
 import homepic4 from './img/homepic4.png'
 import museumGif from './img/museumGif.gif'
+import slickA1 from './img/slickA1.jpeg'
+import slickA2 from './img/slickA2.jpeg'
+import slickA3 from './img/slickA3.jpeg'
+import slickA4 from './img/slickA4.jpeg'
+import slickA5 from './img/slickA5.jpeg'
+import slickA6 from './img/slickA6.jpeg'
+import slickA7 from './img/slickA7.jpeg'
 
 function Product() {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    rtl: true,
+  }
+
   return (
     <>
       <div className="pro-outside">
@@ -94,7 +117,6 @@ function Product() {
           alt=""
         />
       </div>
-
       {/* -----------------------首頁第一頁 以下第二頁 */}
       <div className="pro-outside2">
         <div className="row">
@@ -115,6 +137,80 @@ function Product() {
             </div>
           </div>
           <div className="homepageB-right"> </div>
+        </div>
+      </div>
+      {/* --------------------------homepage3 */}
+      <div className="pro-outside3">
+        <div className="homepage3 d-flex">
+          <div className="homepage3-NewArrival-box">
+            <h1 className="homepage3-NewArrival">
+              NEW <br />
+              ARRIVAL
+            </h1>
+          </div>
+          <div className="homepage3-left-box">
+            <p className="homepage3-word">
+              藝術———新品上架
+              <br />
+              <Link
+                to="/"
+                style={{
+                  color: 'black',
+                  textDecoration: 'none',
+                }}
+                className="homepage3-more"
+              >
+                MORE
+              </Link>
+            </p>
+          </div>
+
+          <div className="homepage3-right-box">
+            <div className="pro-arrow">
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+              <MdKeyboardArrowRight size={70} />
+            </div>
+            <div className="slickWidth">
+              <Slider {...settings}>
+                <div className="slickPic">
+                  <div className="slickPic-box">
+                    <img src={slickA1} alt="" />
+                  </div>
+                </div>
+                <div className="slickPic">
+                  <div className="slickPic-box">
+                    <img src={slickA2} alt="" />
+                  </div>
+                </div>
+                <div className="slickPic">
+                  <div className="slickPic-box">
+                    <img src={slickA3} alt="" />
+                  </div>
+                </div>
+                <div className="slickPic">
+                  <div className="slickPic-box">
+                    <img src={slickA4} alt="" />
+                  </div>
+                </div>
+                <div className="slickPic">
+                  <div className="slickPic-box">
+                    <img src={slickA5} alt="" />
+                  </div>
+                </div>
+                <div className="slickPic">
+                  <div className="slickPic-box">
+                    <img src={slickA6} alt="" />
+                  </div>
+                </div>
+              </Slider>
+            </div>
+          </div>
         </div>
       </div>
     </>
