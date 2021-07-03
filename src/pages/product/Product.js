31f7837735dbf038ca67ce88b4567db55b71c59d
tascup1 <70184088+tascup1@users.noreply.github.com>
2021-07-03 17:34:08 +0800

    updat

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -431,14 +431,13 @@ function Product() {
       </div>
       {/* ------------------homepage5------ */}
       <div className="pro-outside5">
-        <div className="pro-gray5">
-          <div className="homepage5-word">
-            <h1>PRODUCT</h1>
-          </div>
-          <div className="homepage5-wordB">
-            <h1> CATEGORY</h1>
-          </div>
+        <div className="homepage5-word">
+          <h1>PRODUCT</h1>
         </div>
+        <div className="homepage5-wordB">
+          <h1> CATEGORY</h1>
+        </div>
+        <div className="pro-gray5"></div>
       </div>
       {/* ------------------homepage6------ */}
       <div className="pro-outside6">
