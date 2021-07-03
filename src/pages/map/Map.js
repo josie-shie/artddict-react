8c9d22d1c0e553df7866cdc5b28027ae109dad37
josie-shie <joe003218@gmail.com>
2021-07-04 03:02:33 +0800

    map

--- src/pages/map/Map.js
+++ src/pages/map/Map.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../pics/logo-bk.svg'
 import './map.scss'
 import LeafLet from './components/LeafLet'
 import MarqueeMap from './components/MarqueeMap'
+
+import { MdLocationSearching } from 'react-icons/md'
+import { HiSearch } from 'react-icons/hi'
 
 const Map = () => {
   return (
@@ -13,6 +15,7 @@ const Map = () => {
         <MarqueeMap />
         <div className="d-flex">
           <div className="col-8 d-flex flex-column px-0">
+<<<<<<< Updated upstream
             <div className="map-search-bar d-flex">
               <div className="map-select">
                 <div className="pr-3">地區</div>
@@ -20,16 +23,52 @@ const Map = () => {
                   className="ed-select pl-3"
                   name=""
                   id=""
+=======
+            <div className="map-search-bar d-flex align-items-center justify-content-center py-2">
+              <div className="map-select-box px-4">
+                地區
+              </div>
+              <select
+                className="map-select-box map-select pl-3 border-left-0 "
+                name=""
+                id=""
+              >
+                <option
+                  style={{ color: '#707070' }}
+                  value=""
+>>>>>>> Stashed changes
                 >
-                  <option
-                    style={{ color: '#707070' }}
-                    value=""
-                  >
-                    請選擇
-                  </option>
-                  <option value="">123</option>
-                  <option value="">123</option>
-                </select>
+                  請選擇
+                </option>
+                <option value="">123</option>
+                <option value="">123</option>
+              </select>
+              <div className="map-select-box ml-5 px-4">
+                搜尋
+              </div>
+              <div className="pl-5 ml-5 mr-4">
+                <MdLocationSearching
+                  size={30}
+                  color={'#81FC4D'}
+                />
+              </div>
+              <div className="map-search mr-2">
+                <HiSearch size={30} color={'#81FC4D'} />
+                <div className="">
+                  <form action="" autocomplete="on">
+                    <input
+                      id="search"
+                      name="map-search"
+                      type="text"
+                      placeholder="Search somthing ?"
+                    />
+                    <input
+                      id="search-submit"
+                      value="Rechercher"
+                      type="submit"
+                    />
+                  </form>
+                </div>
               </div>
             </div>
             <LeafLet className="  col-12" />
