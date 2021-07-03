58970d78b318ebae7a1a8199aac05f0926fa5335
KyDevtw <k3030789@gmail.com>
2021-07-03 16:14:07 +0800

    BreadCrumb done

--- src/pages/event/EventList.js
+++ src/pages/event/EventList.js
@@ -1,6 +1,6 @@
 import { React, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Container, Row, Nav } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import $ from 'jquery'
 
 // Component
---
+++ src/pages/event/config/index.js
@@ -0,0 +1,21 @@
+export const debug = true
+// 測試開發/營運網址
+export const devUrl = 'http://localhost:3000'
+export const prodUrl = 'http://www.abc.com'
+
+// 愈長的寫愈後面
+export const pathnameList = [
+  '/event',
+  '/event/event-list',
+  '/event/event-list/detail',
+  '/event/event-list/detail/share',
+  '/event/event-list/detail/upload',
+]
+
+export const pathnameTextList = [
+  '/活動主頁',
+  '/活動主頁/活動列表',
+  '/活動主頁/活動列表/活動細節',
+  '/活動主頁/活動列表/分享',
+  '/活動主頁/活動列表/上傳',
+]
