022175e2eeb4fc0ce698ac355533409601c8c964
josie-shie <joe003218@gmail.com>
2021-07-03 20:00:55 +0800

    map

---
+++ src/pages/map/components/MarqueeMap.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GiPikeman } from 'react-icons/gi'
+import { RiArrowLeftSLine } from 'react-icons/ri'
+
+const MarqueeMap = () => {
+    return (
+        <>
+            <marquee
+          scrollamount="10"
+          className="event-marquee px-3"
+        >
+          <h1 className="notoSansTC-md my-3">
+            探索美學 從周圍開始 <GiPikeman />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine /> 探索美學 從周圍開始{' '}
+            <GiPikeman />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine /> 探索美學 從周圍開始{' '}
+            <GiPikeman />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+            <RiArrowLeftSLine />
+          </h1>
+        </marquee>
+        </>
+    );
+}
+
+export default MarqueeMap;
