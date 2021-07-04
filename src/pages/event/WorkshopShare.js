d44588cfe00559a5c67107efa2eff9ae803beef3
KyDevtw <k3030789@gmail.com>
2021-07-04 11:58:22 +0800

    share done

--- src/pages/event/WorkshopShare.js
+++ src/pages/event/WorkshopShare.js
@@ -26,6 +26,7 @@ import EuListCardPic from './images/event/006.jpg'
 
 import './style/reset.css'
 import './style/fontAndBtn.scss'
+import './style/WorkshopShare.scss'
 
 function WorkshopShare() {
   return (
@@ -37,23 +38,23 @@ function WorkshopShare() {
             <BreadCrumb />
           </Row>
           <Row className="both-padding">
-            <div className="eu-pic-up col-12 p-0 mb-4">
+            <div className="es-pic-up col-12 p-0 mb-4">
               <h5 className="cn-font">主題工作坊</h5>
               <h2>我是活動名稱</h2>
             </div>
             {/* 資料form表單 */}
             <div className="col-12 pl-0 justify-content-between d-flex flex-wrap">
-              <h5 className="eu-upload-title col-12 p-0 cn-font mb-3">
+              <h5 className="es-work-title col-12 p-0 cn-font mb-3">
                 作品圖片:
               </h5>
               <div className="col-8 p-0 justify-content-start d-flex flex-wrap">
-                <div className="eu-pic-left col-8 p-0 mr-3">
+                <div className="es-pic-left col-8 p-0 mr-3">
                   <div>
                     <img src={Square} alt="" />
                   </div>
                 </div>
 
-                <div className="eu-pic-right col-3 p-0">
+                <div className="es-pic-right col-3 p-0">
                   <div className="col-12 p-0 d-flex flex-wrap">
                     <div className="col-10 p-0">
                       <img src={Square} alt="" />
@@ -66,29 +67,20 @@ function WorkshopShare() {
                     </div>
                   </div>
                 </div>
-                <h5 className="eu-upload-title col-12 p-0 cn-font mt-5 mb-3">
+                <h5 className="es-work-title col-11 p-0 cn-font mt-2">
                   作品說明:
                 </h5>
-                <textarea
-                  className="eu-text col-11 pr-2"
-                  name=""
-                  id=""
-                  cols="30"
-                  rows="10"
-                ></textarea>
-
-                <div className="col-11 d-flex flex-wrap justify-content-center my-4">
-                  <button
-                    className="eu-send-cmt e-btn-m col-l2 my-3"
-                    type="submit"
-                  >
-                    上傳作品
-                  </button>
-                </div>
+                <p
+                  className="es-text cn-font col-11 p-0 pr-2"
+                >
+                  1889年9月，荷蘭後印象派畫家文森特·梵谷（Vincent
+                  van
+                  Gogh）在畫布上用油畫了自畫像。這幅作品可能是梵谷的最後一幅自畫像，是在他離開法國南部聖雷米的普羅旺斯之前不久畫的。這幅畫現在在巴黎的奧賽博物館（Muséed'Orsay）展出。
+                </p>
               </div>
 
               {/* 其他活動區塊 */}
-              <div className="eu-more-event col-4">
+              <div className="es-more-event col-4">
                 <h2 className="my-3 col-12 mb-3">
                   其他活動!
                   <span>
@@ -99,7 +91,7 @@ function WorkshopShare() {
                   </span>
                 </h2>
 
-                <div className="eu-list-card col-12 pt-5 mb-5">
+                <div className="es-list-card col-12 pt-5 mb-5">
                   <img
                     className="col-12 p-0"
                     src={EuListCardPic}
@@ -116,24 +108,7 @@ function WorkshopShare() {
                   </div>
                 </div>
 
-                <div className="eu-list-card col-12 pt-5 mb-5">
-                  <img
-                    className="col-12 p-0"
-                    src={EuListCardPic}
-                    alt=""
-                  />
-                  <h6 className="col-12 p-0 cn-font my-2">
-                    我是活動標題
-                  </h6>
-                  <div className="d-flex">
-                    <div className="col-8 p-0">
-                      <p>地點：台北市</p>
-                      <p>時間：JUN</p>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="eu-list-card col-12 pt-5 mb-5">
+                <div className="es-list-card col-12 pt-5 mb-5">
                   <img
                     className="col-12 p-0"
                     src={EuListCardPic}
--- src/pages/event/WorkshopUpload.js
+++ src/pages/event/WorkshopUpload.js
@@ -22,6 +22,8 @@ import Square from './images/square.gif'
 // Pictures
 import EuListCardPic from './images/event/006.jpg'
 
+import EuGreySpin from './images/arddict-circle-gr.svg'
+
 
 
 import './style/reset.css'
@@ -33,8 +35,8 @@ function WorkshopUpload() {
     <>
       <div className="reduce-width">
         <Lightheader />
-        <Container className="e-share " fluid>
-          <Row className="py-4 es-bread both-padding">
+        <Container className="e-upload " fluid>
+          <Row className="py-4 eu-bread both-padding">
             <BreadCrumb />
           </Row>
           <Row className="both-padding">
@@ -51,46 +53,62 @@ function WorkshopUpload() {
                 className="col-8 p-0 justify-content-start d-flex flex-wrap"
                 action=""
               >
-                
-                  <div className="eu-pic-left col-8 p-0 mr-3">
-                    <div>
+                <div className="eu-pic-left col-8 p-0 mr-3">
+                  <div className="position-relative">
+                    <img
+                      className="up-spin position-absolute"
+                      src={EuGreySpin}
+                      alt=""
+                    />
+                    <button className="e-btn-m cn-font position-absolute">
+                      上傳圖片
+                      <IoMdAdd />
+                    </button>
+                    <img src={Square} alt="" />
+                  </div>
+                </div>
+
+                <div className="eu-pic-right col-3 p-0">
+                  <div className="col-12 p-0 d-flex flex-wrap">
+                    <div className="col-10 p-0 position-relative">
+                      <button className="position-absolute">
+                        <IoMdAdd />
+                      </button>
+                      <img src={Square} alt="" />
+                    </div>
+                    <div className="col-10 my-2 p-0">
+                      <button className="position-absolute">
+                        <IoMdAdd />
+                      </button>
+                      <img src={Square} alt="" />
+                    </div>
+                    <div className="col-10 p-0">
+                      <button className="position-absolute">
+                        <IoMdAdd />
+                      </button>
                       <img src={Square} alt="" />
                     </div>
                   </div>
+                </div>
+                <h5 className="eu-upload-title col-12 p-0 cn-font mt-5 mb-3">
+                  作品說明:
+                </h5>
+                <textarea
+                  className="eu-text col-11 pr-2"
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                ></textarea>
 
-                  <div className="eu-pic-right col-3 p-0">
-                    <div className="col-12 p-0 d-flex flex-wrap">
-                      <div className="col-10 p-0">
-                        <img src={Square} alt="" />
-                      </div>
-                      <div className="col-10 my-2 p-0">
-                        <img src={Square} alt="" />
-                      </div>
-                      <div className="col-10 p-0">
-                        <img src={Square} alt="" />
-                      </div>
-                    </div>
-                  </div>
-                  <h5 className="eu-upload-title col-12 p-0 cn-font mt-5 mb-3">
-                    作品說明:
-                  </h5>
-                  <textarea
-                    className="eu-text col-11 pr-2"
-                    name=""
-                    id=""
-                    cols="30"
-                    rows="10"
-                  ></textarea>
-
-                  <div className="col-11 d-flex flex-wrap justify-content-center my-4">
-                    <button
-                      className="eu-send-cmt e-btn-m col-l2 my-3"
-                      type="submit"
-                    >
-                      上傳作品
-                    </button>
-                  </div>
-               
+                <div className="col-11 d-flex flex-wrap justify-content-center my-4">
+                  <button
+                    className="eu-send-cmt e-btn-m col-l2 my-3"
+                    type="submit"
+                  >
+                    上傳作品
+                  </button>
+                </div>
               </form>
 
               {/* 其他活動區塊 */}
