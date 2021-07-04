3f9f2fe10e942b4f2d64e9c4ca35e1eef60b2bc9
tascup1 <70184088+tascup1@users.noreply.github.com>
2021-07-04 16:17:00 +0800

    update

--- src/pages/product/ProductDetail.js
+++ src/pages/product/ProductDetail.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../../bootstrap/css/bootstrap.css'
+import {} from 'react-bootstrap'
+import './style/ProductDetail.css'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import Slider from 'react-slick'
+// -----------svg---------
+import {
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoIosHeart,
+} from 'react-icons/io'
+// ---------picture------------
 
 function ProductDetail() {
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <div></div>
-          </div>
-        </div>
-      </div>
+      <div className="proDe-full">123</div>
     </>
   )
 }
