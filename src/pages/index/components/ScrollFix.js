a466b7731a76150c33d2b1c6fcfe77ea8acbbfb5
josie-shie <joe003218@gmail.com>
2021-07-06 02:02:41 +0800

    scrollBtn / Artist

---
+++ src/pages/index/components/ScrollFix.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const ScrollFix = () => {
+
+    const [device,setDevice]=useState("mobile");
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ScrollFix;
