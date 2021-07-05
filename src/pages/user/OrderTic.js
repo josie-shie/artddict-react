93e63fa9e8418641cc0ab39fce3527a60dc0b181
chun2332 <smile84122332@gmail.com>
2021-07-06 00:17:41 +0800

    fix scss

--- src/pages/user/OrderTic.js
+++ src/pages/user/OrderTic.js
@@ -53,16 +53,12 @@ function OrderTic() {
               <div className="u-orderType">訂單狀態</div>
               <div className="u-bt col-2"></div>
             </div>
-            <div className="u-tb d-flex justify-content-around mt-5">
-              <div className="u-orderId mt-4">訂單編號</div>
-              <div className="u-orderDate mt-4">
-                訂單日期
-              </div>
-              <div className="u-payType mt-4">付款狀態</div>
-              <div className="u-price mt-4">總價</div>
-              <div className="u-orderType mt-4">
-                訂單狀態
-              </div>
+            <div className="u-tb d-flex justify-content-around">
+              <div className="u-ordrtInput1">訂單編號</div>
+              <div className="u-ordrtInput1">訂單日期</div>
+              <div className="u-ordrtInput2">付款狀態</div>
+              <div className="u-ordrtInput3">總價</div>
+              <div className="u-ordrtInput4">訂單狀態</div>
               <div className="u-bt col-2">
                 <div className="u-Bbtn">
                   <button class="btn btn btn-dark">
