aeb9ed9f3381e1139d913a00126c8f9c9ebb82ed
richard85501 <liuchang85501@gmail.com>
2021-07-05 15:57:15 +0800

    add useAuction

--- src/pages/Auction/userAuctionOver.js
+++ src/pages/Auction/userAuctionOver.js
@@ -28,6 +28,59 @@ function userAuctionOver(props) {
                         </div>
                     </div>
                 </div>
+                <div className="uAucO-main">
+                    <div className="uAucO-picture">
+
+                    </div>
+                    <div className="uAucO-productState">
+                        <ul>
+                            <li>競標編號:A956722</li>
+                            <li>競標商品：BTS 麥當勞飲料杯 防彈少年團聯名 不防彈款</li>
+                            <li>您的出價：NT$ 340</li>
+                        </ul>
+                    </div>
+                    <div className="uAucO-btnf">
+                        <button className="uAucO-btnA">
+                            拍賣品細節
+                        </button>
+                        <button className="uAucO-btnB">
+                            訂單明細
+                        </button>
+                    </div>
+                </div>
+                <div className="uAucO-detail">
+                    <div className="uAucO-detail-Title">
+                        訂單明細
+                    </div>
+                    <div className="uAuc-detail-contentA">
+                        <ul>
+                            <li>競標編號</li>
+                            <li>得標時間</li>
+                            <li>收件人</li>
+                            <li>手機編號</li>
+                            <li>訂單狀態</li>
+                        </ul>
+                        <ul>
+                            <li>A956722</li>
+                            <li>2021-03-22</li>
+                            <li>gy哥</li>
+                            <li>09123456789</li>
+                            <li>帶取貨</li>
+                        </ul>
+                        <ul>
+                            <li>付款方式</li>
+                            <li>付款狀態</li>
+                            <li>運費</li>
+                            <li>總金額</li>
+                        </ul>
+                        <ul>
+                            <li>信用卡</li>
+                            <li>已付款</li>
+                            <li>NT$ 80</li>
+                            <li>NT$ 860</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     );
