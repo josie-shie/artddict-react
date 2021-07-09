ad39aacf7aadb4c298c6486e60a8a2dff36371d7
richard85501 <liuchang85501@gmail.com>
2021-07-09 18:29:56 +0800

    auction排序查詢完成

--- src/pages/Auction/Auction.js
+++ src/pages/Auction/Auction.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style/auction.scss'
 import { withRouter, Link } from 'react-router-dom'
 
@@ -27,16 +27,125 @@ import {
 
 function Auction(props) {
 
-    const [search,setSearch] = useState('')
-    const [arrangement ,setArrangement] = useState('')
+    const [search, setSearch] = useState('')
+    const [arrangement, setArrangement] = useState('')
+    const [count, setCount] = useState(0)
+    const [start, setStart] = useState(false)
+    const [aucInfo, setAucInfo] = useState([])
+    const [aucRemainT, setAucRemainT] = useState([])
+
+
 
     //選擇排列順序
-    const handleChangeSelect = (e) =>{
+    const handleChangeSelect = (e) => {
         // console.log("選擇的順序",e.target.value)
         setArrangement(e.target.value)
     }
+
+    //初始化資料
+    async function getAucProFromServer() {
+
+        // 連接的伺服器資料網址
+        const url = 'http://localhost:6005/auctoin/auction-list'
+
+        // 注意header資料格式要設定，伺服器才知道是json格式
+        const request = new Request(url, {
+            method: 'GET',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'appliaction/json',
+            }),
+        })
+        const response = await fetch(request)
+        const data = await response.json()
+
+        // 設定資料
+        setAucInfo(data)
+    }
+
+    //排列查詢
+    async function getAucProArrFromServer() {
+
+        // 連接的伺服器資料網址
+        const url = 'http://localhost:6005/auctoin/aucSeaArr' + `?search=${search}` + `&arrangement=${arrangement}`
+        // 注意header資料格式要設定，伺服器才知道是json格式
+        const request = new Request(url, {
+            method: 'GET',
+            headers: new Headers({
+                Accept: 'application/json',
+                'Content-Type': 'appliaction/json',
+            }),
+        })
+
+        const response = await fetch(request)
+        const data = await response.json()
+        // 設定資料
+        setAucInfo(data)
+    }
+
+    useEffect(() => {
+        getAucProFromServer()
+    }, [])
+
+    useEffect(() => {
+        getAucProArrFromServer()
+    }, [search, arrangement])
+
+    
+    const TimeRemaining = (deadline) => {
+        //截止時間(毫秒)
+        // 將資料拿到資料庫截止日期 從字串轉換成數字
+        const deadlineMin = parseInt(deadline)
+        //現在時間(毫秒)
+        const timeRightNowA = new Date()
+        const timeRightNow = timeRightNowA.getTime()
+
+        const TimeRemaining = deadlineMin - timeRightNow
+
+        const seconds = Math.floor((TimeRemaining / 1000) % 60);
+        const minutes = Math.floor((TimeRemaining / 1000 / 60) % 60);
+        const hours = Math.floor((TimeRemaining / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(TimeRemaining / (1000 * 60 * 60 * 24));
+        // console.log(days, "天", hours, '小時', minutes, '分鐘', seconds, '秒')
+        if(TimeRemaining<0){
+            return '競標結束'
+        }
+
+        if (days > 0)
+            return `${days}天${hours}小時${seconds}`
+        if (days < 1)
+            return `${hours}小時${minutes}分鐘${seconds}`
+
+        // return [days, hours, minutes, seconds]
+
+    }
+
+    useEffect(() => {
+        let remainTime = []
+        if (aucInfo.length > 0) {
+            // console.log(aucInfo)
+            // console.log(aucInfo.length)
+            // console.log(aucInfo[0].aucDeadline)
+            for (let i = 0; i < (aucInfo.length); i++) {
+                // console.log(aucInfo[0].aucDeadline)
+                let deadlineA = new Date(aucInfo[i].aucDeadline).getTime()
+                remainTime.push(TimeRemaining(deadlineA))
+            }
+
+            setAucRemainT(remainTime)
+        }
+        let test = setInterval(() => {
+            setCount(count => count + 1)
+        }, 1000)
+        console.log('+++++++++++++++++++++++', count)
+        return () => clearInterval(test)
+    }, [count])
+
+    // 一開始就會開始載入資料
+
     return (
         <>
+        {console.log('aa')}
             <div className="auctionPage">
                 <div className="auctionHeroSection">
                     <div className="auctionHeroSectionLogo">
@@ -55,7 +164,7 @@ function Auction(props) {
                         T
                     </div>
                     <div className="auctiondecorationR auctiondecoration eng-font-bold">
-                        R 
+                        R
                     </div>
                 </div>
                 <marquee>
@@ -75,16 +184,16 @@ function Auction(props) {
                         <div className="auctionMainFilter">
                             <div className="auctionMainFilterLeft">
                                 <div className="auctionBreadcrumb">
-                                    <AucBreadcrumb/>
+                                    <AucBreadcrumb />
                                 </div>
                                 <div className="auctionArrangement cn-font">
                                     排列
                                 </div>
                                 <div className="auctionArrangementInput">
-                                    <select 
-                                        className="auc-select pl-1 cn-font" 
-                                        onChange ={handleChangeSelect}
-                                        name="name" 
+                                    <select
+                                        className="auc-select pl-1 cn-font"
+                                        onChange={handleChangeSelect}
+                                        name="name"
                                         id="name">
                                         <option style={{ color: '#707070' }} value="">
                                             推薦
@@ -98,45 +207,51 @@ function Auction(props) {
                                 <button className="auc-Search">
                                     <IoIosSearch />
                                 </button>
-                                <input 
+                                <input
                                     className="auc-Searchbox"
-                                    value = {search}
-                                    onChange ={(event)=>{setSearch(event.target.value)} }   
+                                    value={search}
+                                    onChange={(event) => { setSearch(event.target.value) }}
                                 />
-                                    <div className="auctionNumberOfProduct cn-font">
-                                        Showing 1-9 of 27
-                                    </div>
-                            </div>
-                                <div className="auctionMainFilterRight">
-                                    <div>price range</div>
-                                    <AuctionSlider />
+                                <div className="auctionNumberOfProduct cn-font">
+                                    Showing 1-9 of 27
                                 </div>
                             </div>
-                            <div className="auctionMainContent cn-font">
-                                <div className="auctionsidebar cn_font">
-                                    <div className="auctionsidebarTitle ">美術館商品</div>
-                                    <ul>
-                                        <li>畫作</li>
-                                        <li>服飾</li>
-                                        <li>家飾</li>
-                                        <li>文具</li>
-                                        <li>書籍</li>
-                                        <li>配件</li>
-                                    </ul>
-                                </div>
-                                <AuctionProductList
-                                    // data={data}
-                                    search={search}
-                                    setSearch={setSearch}
-                                    arrangement={arrangement}
-                                />
+                            <div className="auctionMainFilterRight">
+                                <div>price range</div>
+                                <AuctionSlider />
                             </div>
+                        </div>
+                        <div className="auctionMainContent cn-font">
+                            <div className="auctionsidebar cn_font">
+                                <div className="auctionsidebarTitle ">美術館商品</div>
+                                <ul>
+                                    <li>畫作</li>
+                                    <li>服飾</li>
+                                    <li>家飾</li>
+                                    <li>文具</li>
+                                    <li>書籍</li>
+                                    <li>配件</li>
+                                </ul>
+                            </div>
+                            <AuctionProductList
+                                // data={data}
+                                search={search}
+                                setSearch={setSearch}
+                                arrangement={arrangement}
+                                getAucProFromServer={getAucProFromServer}
+                                getAucProArrFromServer={getAucProArrFromServer}
+                                aucInfo={aucInfo}
+                                setAucInfo={setAucInfo}
+                                TimeRemaining={TimeRemaining}
+                                aucRemainT={aucRemainT}
+                            />
                         </div>
                     </div>
                 </div>
+            </div>
 
         </>
-            );
+    );
 }
 
-            export default withRouter(Auction);
+export default withRouter(Auction);
--- src/pages/Auction/components/AuctionProductList.js
+++ src/pages/Auction/components/AuctionProductList.js
@@ -8,111 +8,29 @@ import AuctionProductCard from './AuctionProductCard'
 
 function AuctionProductList(props) {
 
-    const { search, setSearch, arrangement } = props
-    //用來放資料
-    const [aucInfo, setAucInfo] = useState([])
-    const [a,seta] = useState('')
-
-    async function getAucProFromServer() {
-
-        // 連接的伺服器資料網址
-        const url = 'http://localhost:6005/auctoin/auction-list'
-
-        // 注意header資料格式要設定，伺服器才知道是json格式
-        const request = new Request(url, {
-            method: 'GET',
-            headers: new Headers({
-                Accept: 'application/json',
-                'Content-Type': 'appliaction/json',
-            }),
-        })
-
-        const response = await fetch(request)
-        const data = await response.json()
-        // 設定資料
-        setAucInfo(data)
-    }
-
-    async function getAucProArrFromServer() {
-
-        // 連接的伺服器資料網址
-        const url = 'http://localhost:6005/auctoin/aucSeaArr' + `?search=${search}` + `&arrangement=${arrangement}`
-
-        // 注意header資料格式要設定，伺服器才知道是json格式
-        const request = new Request(url, {
-            method: 'GET',
-            headers: new Headers({
-                Accept: 'application/json',
-                'Content-Type': 'appliaction/json',
-            }),
-        })
-
-        const response = await fetch(request)
-        const data = await response.json()
-        console.log('發送資料時的a',a)
-        console.log('收到的資料',data,"AUCINFO",aucInfo)
-        // 設定資料
-        setAucInfo(data)
-        console.log('收到的資料a',data,"AUCINFO666",aucInfo)
-    }
-
-    // 一開始就會開始載入資料
-    useEffect(() => {
-        getAucProFromServer()
-    }, [])
-
-    useEffect(() => {
-        getAucProArrFromServer()
-        // console.log('呼叫資料')
-    }, [search,arrangement])
-
-    // useEffect(() => {
-    //     getAucProArrFromServer()
-    // }, [arrangement])
-
-
-
-    function TimeRemaining(deadline) {
-        //截止時間(毫秒)
-        // 將資料拿到資料庫截止日期 從字串轉換成數字
-        const deadlineMin = parseInt(deadline)
-
-        //現在時間(毫秒)
-        const timeRightNowA = new Date()
-        const timeRightNow = timeRightNowA.getTime()
-
-        const TimeRemaining = deadlineMin - timeRightNow
-
-        const seconds = Math.floor((TimeRemaining / 1000) % 60);
-        const minutes = Math.floor((TimeRemaining / 1000 / 60) % 60);
-        const hours = Math.floor((TimeRemaining / (1000 * 60 * 60)) % 24);
-        const days = Math.floor(TimeRemaining / (1000 * 60 * 60 * 24));
-        // console.log(days, "天", hours, '小時', minutes, '分鐘', seconds, '秒')
-
-
-        return [days, hours, minutes, seconds]
-
-    }
-
+    const { search, setSearch, arrangement,getAucProFromServer,getAucProArrFromServer,aucInfo,setAucInfo, TimeRemaining,aucRemainT} = props
+    console.log(aucRemainT)
     return (
         <>  
         {/* {console.log('-----------------------------')} */}
             <div className="container">
                 <div className="row">
-                    {console.log("render時商品名稱",aucInfo)}
-                    {/* const eventDisplay = events.map((event) => { */}
-                    {aucInfo.map((auctionProduct, index) => (
+                    {/* {console.log("render時商品名稱",aucInfo)} */}
+                    {aucInfo.map((auctionProduct,i) => (
+                        <span key={i} className="d-flex col-4">
                         <AuctionProductCard
                             aucId={auctionProduct.aucId}
                             aucName={auctionProduct.aucName}
                             aucPriceNow={auctionProduct.aucPriceNow}
-                            deadline={auctionProduct.aucDeadline}
+                            deadline={aucRemainT[i]}
                             aucImg={auctionProduct.aucImg}
                             TimeRemaining={TimeRemaining}
                             search={search}
                             arrangement={arrangement}
                             aucInfo={aucInfo}
+                            
                         />
+                        </span>
                     ))}
                 </div>
             </div>
