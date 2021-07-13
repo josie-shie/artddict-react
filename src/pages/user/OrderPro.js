286d4ef7c6b3e86fdda40f502461b4aef2d0bca2
chun2332 <smile84122332@gmail.com>
2021-07-13 15:28:47 +0800

    update

--- src/pages/user/OrderPro.js
+++ src/pages/user/OrderPro.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 import './styles/OrderPro.scss'
 import Menu from './components/Menu'
 import { Link } from 'react-router-dom'
@@ -7,6 +7,37 @@ import Breadcrumb from './components/UserBreadcrumb'
 import { Container } from 'react-bootstrap'
 
 function OrderPro() {
+  const [orderId, setOrderId] = useState('')
+  const [orderDate, setOrderDate] = useState('')
+  const [orderPrice, setOrderPrice] = useState('')
+  const [orderStatus, setOrderStatus] = useState('')
+  const [id, setId] = useState('')
+
+  async function getUserOrderIdServer(userid) {
+    const url =
+      'http://localhost:6005/users/getOrder/' + userid
+    const request = new Request(url, {
+      method: 'GET',
+      headers: new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'appliaction/json',
+      }),
+    })
+
+    const response = await fetch(request)
+    const data = await response.json()
+
+    setOrderId(data.orderId)
+    setOrderDate(data.orderDate)
+    setOrderPrice(data.orderPrice)
+    setOrderStatus(data.orderStatus)
+    setId(data.setId)
+  }
+
+  useEffect(() => {
+    getUserOrderIdServer()
+  }, [])
+
   return (
     <>
       <div className="u-body">
@@ -20,7 +51,7 @@ function OrderPro() {
         <Container fluid>
           <div className="u-row d-flex justify-content-around">
             <div className="u-userPro">
-              <Link to="/user-orderpro">商品</Link>
+              <Link to={`/user-orderpro/${id}`}>商品</Link>
             </div>
             <div className="u-userTic">
               <Link to="/user-ordertic">票券</Link>
@@ -54,47 +85,13 @@ function OrderPro() {
               <div class="u-bt col-2"></div>
             </div>
             <div class="u-tb d-flex justify-content-around">
-              <div class="u-ordrtInput1">訂單編號</div>
-              <div class="u-ordrtInput2">訂單日期</div>
-              <div class="u-ordrtInput3">付款狀態</div>
-              <div class="u-ordrtInput4">總價</div>
-              <div class="u-ordrtInput5 ">訂單狀態</div>
-              <div class="u-bt col-2">
-                <div className="u-Bbtn">
-                  <button class="btn btn btn-dark">
-                    <Link
-                      className="u-link"
-                      to="/user-orderpro/detail"
-                      style={{ textDecoration: 'none' }}
-                    >
-                      詳細資料
-                    </Link>
-                  </button>
-                </div>
-                <div className="u-Lbtn">
-                  <button class="btn btn btn-light">
-                    取消
-                  </button>
-                </div>
+              <div class="u-ordrtInput1">{orderId}</div>
+              <div class="u-ordrtInput2">{orderDate}</div>
+              <div class="u-ordrtInput3">已付款</div>
+              <div class="u-ordrtInput4">{orderPrice}</div>
+              <div class="u-ordrtInput5 ">
+                {orderStatus}
               </div>
-            </div>
-          </div>
-
-          <div class="u-table">
-            <div class="u-th d-flex justify-content-around">
-              <div class="u-orderId">訂單編號</div>
-              <div class="u-orderDate">訂單日期</div>
-              <div class="u-payType">付款狀態</div>
-              <div class="u-price">總價</div>
-              <div class="u-orderType">訂單狀態</div>
-              <div class="u-bt col-2"></div>
-            </div>
-            <div class="u-tb d-flex justify-content-around">
-              <div class="u-ordrtInput1">訂單編號</div>
-              <div class="u-ordrtInput2">訂單日期</div>
-              <div class="u-ordrtInput3">付款狀態</div>
-              <div class="u-ordrtInput4">總價</div>
-              <div class="u-ordrtInput5 ">訂單狀態</div>
               <div class="u-bt col-2">
                 <div className="u-Bbtn">
                   <button class="btn btn btn-dark">
