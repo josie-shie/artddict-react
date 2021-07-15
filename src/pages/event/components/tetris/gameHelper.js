9d31b7cc732d538fc8fda4dcc63801208e37f2cb
KyDevtw <k3030789@gmail.com>
2021-07-15 22:05:37 +0800

    updated gamehelper

--- src/pages/event/components/tetris/gameHelper.js
+++ src/pages/event/components/tetris/gameHelper.js
@@ -1,11 +1,11 @@
-export const STAGE_WIDTH = 12;
+export const STAGE_WIDTH = 12
 export const STAGE_HEIGHT = 20
 
 export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
     Array(STAGE_WIDTH).fill([0, 'clear'])
   )
-
+  
 export const checkCollision = (
   player,
   stage,
